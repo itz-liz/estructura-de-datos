cf893975fd47069ca099a57e4128f040bf142fd7
ITZEL CORTES AGUIRRE <1724110595@utectulancingo.edu.mx>
2025-09-15 14:16:38 +0000

    ejercicios

---
+++ 1724110595Ejercicio3.py
@@ -0,0 +1,16 @@
+# Itzel Cortes Aguirre - Ejercicio 3
+# desarrollle un algoritmo recursivo que recibe una cadena de texto 
+# y devuelva la misma cadena invertida, si la cadena es vacia o tiene 
+# solo un carácter devolver de la misma manera 
+
+z = str(input("Inserte una palabra para invertir:"))#str es para definir una cadena, en input ponemos el input 
+print (f"La palabra que ingresaste: {z}")
+
+if len(z) == 1 or len(z) == 0: #comparamos la longitud de la cadena
+    print (f"Tu palabra es muy corta o esta vacia, se imprime la misma palabra:{z}") #mandamos el mensaje de error 
+else:
+        palabra_invertida = z [::-1] #los dos puntos es para contar los elementos y el -1 recorre la secuencia de manera inversa
+        print (f"La palabra inversa es: {palabra_invertida}") #imprime la palabra inversa
+
+
+
